3388c6911416ad955f47a664aa83c59fc6b4c051
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:00:44 +0300

    Добавьте файлы проекта.

---
+++ z7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static int[] MergeArrays(int[] arr1, int[] arr2)
+    {
+        return arr1.Concat(arr2).ToArray();
+    }
+
+    static void Main()
+    {
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 4, 5, 6 };
+        int[] mergedArray = MergeArrays(array1, array2);
+
+        Console.WriteLine("Объединенный массив:");
+        foreach (int num in mergedArray)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
